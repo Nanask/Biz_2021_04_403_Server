c7b3318ca24a53e0156949160154342b95ad37ec
Nanask <ybhag1005@daum.net>
2021-05-04 16:45:37 +0900

    Server

--- Server_06_Book/src/main/java/com/callor/book/controller/BookRentController.java
+++ Server_06_Book/src/main/java/com/callor/book/controller/BookRentController.java
@@ -2,6 +2,7 @@ package com.callor.book.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
@@ -13,8 +14,11 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.callor.book.model.BookRentDTO;
 import com.callor.book.model.BookRentVO;
+import com.callor.book.model.BuyerDTO;
 import com.callor.book.service.BookRentService;
+import com.callor.book.service.BuyerService;
 import com.callor.book.service.impl.BookRentServiceImplV1;
+import com.callor.book.service.impl.BuyerServiceImplV1;
 
 //DBD형식 도메인와 인터페이스를 만들어 구현하는것
 
@@ -23,75 +27,147 @@ import com.callor.book.service.impl.BookRentServiceImplV1;
  */
 
 @WebServlet("/rent/*")
-public class BookRentController extends HttpServlet  {
+public class BookRentController extends HttpServlet {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 921652892464670154L;
 	protected BookRentService brService;
+	
+	//회원이름을 조회하기 위해 만들기
+	protected BuyerService buService;
 
 	public BookRentController() {
 //		brService = null; // new... 추가하기
 		brService = new BookRentServiceImplV1(); // 변경
+		buService = new BuyerServiceImplV1(); 
 
 	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		// rent/* 로 요청이 되면 *위치에 부착되는 Sub 요청을 분리해낸다.
+		// rent/seq라고 요청을 하면 subPath에는 /seq라는 문자열이 담길 것이다.
 		String subPath = req.getPathInfo();
-		
+
+		// outputStream을 사용하여 문자열 방식으로 응답을 하기 위한 준비
 		resp.setContentType("text/html;charset=UTF-8");
-		
 		PrintWriter out = resp.getWriter();
-		
-		if(subPath.equals("/list")) {
-			// 도서대여 전체목록
-			brService.selectAll();
-			out.println("도서대여 전체목록 보기");
-			
-		}else if(subPath.equals("/seq")) {
+
+		// rent/seq로 요청이 들어오면 반응
+		if (subPath.equals("/seq")) {
 			// 주문번호로 찾기
-			
+
 			// getParameter는 정수형으로 받을 수 없어서 seq를 문자열로 입력받음
 			String strSeq = req.getParameter("id");
+			
+			// 사용자에 대한 1차 반응?
+			if(strSeq == null || strSeq.equals("")) {
+				out.println("주문 번호가 없음");
+				out.close();
+			}
+			
 			Long nSeq = Long.valueOf(strSeq);
 			BookRentDTO brDTO = brService.findById(nSeq);
-			
+
 			// view에서 보여줄 데이터 생성
+			/*
+			 * ServletContext
+			 * Tomcat을 기반으로 작성된 Web App Service에서 요청(Req) 응답(Res)를 총괄하는 정보가 담긴 객체
+			 * Web App Service를 구현하기 위하여 Req, Res를 처리하는 여러가지 기능을 구현해야 하는데
+			 * 그러한 기능을 미리 구현해 놓았기 때문에
+			 * ServletContext를 getter하는 것만으로도 충분하다.
+			 * 
+			 * DB 등 으로부터 조회된 데이터를 Web에게 응답하고자 할때 쉬운 방법으로 전달할 수 있도록 하는 기능이 미리 구현되어 있다.
+			 */
 			ServletContext app = this.getServletContext();
-			app.setAttribute("Book", brDTO);
-			
 			
 			// bService가 return한 brDTO를
 			// app객체에 Book이라는 속성변수로 세팅하기
-			//book.jsp 파일을 읽어서 app에 setting한 BOOK 변수와 함께 Rendering을 하라
-			RequestDispatcher disp = app.getRequestDispatcher("/WEB-INF/views/book.jsp");
-			//Rendering된 view 데이터를 web browser로 response하라
-					disp.forward(req, resp);
+			// app 객체에 Book이라는 객체변수를 생성하고 
+			// 그 BOOK변수에 brDTO 값을 저장한다.
+			// object BOOK = brDTO 이런 형식의 코드가 실행된다.
+			// 세팅된 BOOK 객체변수는 jsp 파일에서 참조하여 값을 표현할 수 있다.
+			app.setAttribute("Book", brDTO);
+
 			
-		}else if(subPath.equals("/isbn")) {
+			// book.jsp 파일을 읽어서 app에 setting한 BOOK 변수와 함께 Rendering을 하라
+			// wepapp/WEB-INF/view/book.jsp 파일을 읽어서 Java코드로 변환하고, 실행할 준비를 하라.
+			RequestDispatcher disp = app.getRequestDispatcher("/WEB-INF/views/book.jsp");
+			// Rendering된 view 데이터를 web browser로 response하라
+			disp.forward(req, resp);
+
+		} else if (subPath.equals("/list")) {
+			// 도서대여 전체목록
+			brService.selectAll();
+			out.println("도서대여 전체목록 보기");
+
+		} else if (subPath.equals("/isbn")) {
 			// 도서코드로 찾기
 			brService.findByBISBN("isbn");
-			
-		}else if(subPath.equals("/buyer")) {
+
+		} else if (subPath.equals("/buyer")) {
 			// 회원코드로 찾기
 			brService.findByBuyerCode("buyercode");
-			
-		}else if(subPath.equals("/rent")) {
+
+			// rent/order로 요청하면 주문서 작성 처음화면 보여주기
+			// 회원이름을 입력하는 화면을 보여주기
+		} else if (subPath.equals("/order")) {
 			// 대여정보 추가, 대여하기
-			BookRentVO bookRentVO = new BookRentVO();
-			brService.insert(bookRentVO);
+			System.out.println("order");
+//			ServletContext app = req.getServletContext();
 			
-		}else if(subPath.equals("/return")) {
+			//ServletContext 필요없이 req로 바로 받을 수 있게 함
+			RequestDispatcher disp = req.getRequestDispatcher("/WEB-INF/views/order.jsp");
+			disp.forward(req, resp);
+			
+		} else if(subPath.equals("/order/page1")) {
+			String bu_name = req.getParameter("bu_name");
+			if(bu_name == null || bu_name.equals("")) {
+				out.println("회원 이름을 반드시 입력해야 합니다.");
+				out.close();
+				
+				//회원이름을 가져오기
+			}else {
+			List<BuyerDTO> buList = buService.findByName(bu_name);
+			
+			// Service에서 전달된 데이터가 잘 왔는지 확인하는 테스트 코드
+			System.out.println("=".repeat(50));
+			for(BuyerDTO d :buList) {
+				System.out.println(d.toString());
+			}
+			System.out.println("=".repeat(50));
+			//test end
+			
+			ServletContext app = req.getServletContext();
+			app.setAttribute("BUYERS", buList);
+			
+			RequestDispatcher disp = req.getRequestDispatcher("/WEB-INF/views/page1.jsp");
+			disp.forward(req, resp);
+			}		
+			} else if(subPath.equals("/order/page2")) {
+				String bu_code = req.getParameter("bu_code");
+				BuyerDTO buyerDTO = buService.findById(bu_code);
+				
+				ServletContext app = req.getServletContext();
+				
+				app.setAttribute("BUYER", buyerDTO);
+				
+				RequestDispatcher disp
+				= req.getRequestDispatcher("/WEB-INF/views/page2.jsp");
+			
+			
+		} else if (subPath.equals("/return")) {
 			// 반납하기
 			BookRentVO bookRentVO = new BookRentVO();
 			brService.update(bookRentVO);
-			
-		}else {
+
+		} else {
 			// 더이상 그만하기
+			//test 코드
+//			out.println("없음");
+//			out.close();
 		}
-		
+
 	}
 
 }
--- Server_06_Book/src/main/java/com/callor/book/service/impl/BookRentServiceImplV1.java
+++ Server_06_Book/src/main/java/com/callor/book/service/impl/BookRentServiceImplV1.java
@@ -12,64 +12,62 @@ import com.callor.book.model.BookRentVO;
 import com.callor.book.persistence.DBContract;
 import com.callor.book.service.BookRentService;
 
-public class BookRentServiceImplV1 implements BookRentService{
-	
+public class BookRentServiceImplV1 implements BookRentService {
+
 	// DB와 연동하기 위해 dbConn 변수를 생성
 	protected Connection dbConn;
-	   public BookRentServiceImplV1() {
-	      dbConn = DBContract.getDBConnection();
-	   }
-	   
-	   /*
-	    * ecception 처리 방법 2가지
-	    * 1. try-catch를 사용하기
-	    * 		exception이 발생할 코드를 감싸서 "직접" exception을 처리하기
-	    * 
-	    * 2. 떠넘기기(던지기)
-	    * 		method throws를 추가하여 호출한 method에게 exception을 되돌려 보내기
-	    * 		현재 method에서 직접 exception을 처리하는 코드가 없어서 코드가 다소 간소화 된다.
-	    * 
-	    * 		호출한 곳에서 대신 exception을 처리한다
-	    */
-	   
-	   // 
-	   protected List<BookRentDTO> select(PreparedStatement pStr) throws SQLException {
-		   List<BookRentDTO> brList = new ArrayList<BookRentDTO>();
-		   
-		   ResultSet rStr = pStr.executeQuery();
-		   while(rStr.next()) {
-			   
-			   BookRentDTO brDTO = new BookRentDTO();
-			   brDTO.setBr_seq(rStr.getLong("주문번호"));
-			   brDTO.setBr_sdate(rStr.getString("대여일"));
-			   
-			   brDTO.setBr_bcode(rStr.getString("회원코드"));
-			   brDTO.setBr_bname(rStr.getString("회원명"));
-			   brDTO.setBr_tel(rStr.getString("회원연락처"));
-			   
-			   brDTO.setBr_isbn(rStr.getString("ISBN"));
-			   brDTO.setBr_title(rStr.getString("도서명"));
-			   
-			   brDTO.setBr_price(rStr.getInt("대여금"));
-			   brDTO.setBr_edate(rStr.getString("반납일"));
-			   
-			   brList.add(brDTO);
-		   }
-		   return brList;
-	   }
-	
+
+	public BookRentServiceImplV1() {
+		dbConn = DBContract.getDBConnection();
+	}
+
+	/*
+	 * ecception 처리 방법 2가지 1. try-catch를 사용하기 exception이 발생할 코드를 감싸서 "직접" exception을
+	 * 처리하기
+	 * 
+	 * 2. 떠넘기기(던지기) method throws를 추가하여 호출한 method에게 exception을 되돌려 보내기 현재 method에서
+	 * 직접 exception을 처리하는 코드가 없어서 코드가 다소 간소화 된다.
+	 * 
+	 * 호출한 곳에서 대신 exception을 처리한다
+	 */
+
+	//
+	protected List<BookRentDTO> select(PreparedStatement pStr) throws SQLException {
+		List<BookRentDTO> brList = new ArrayList<BookRentDTO>();
+
+		ResultSet rStr = pStr.executeQuery();
+		while (rStr.next()) {
+
+			BookRentDTO brDTO = new BookRentDTO();
+			brDTO.setBr_seq(rStr.getLong("주문번호"));
+			brDTO.setBr_sdate(rStr.getString("대여일"));
+
+			brDTO.setBr_bcode(rStr.getString("회원코드"));
+			brDTO.setBr_bname(rStr.getString("회원명"));
+			brDTO.setBr_tel(rStr.getString("회원연락처"));
+
+			brDTO.setBr_isbn(rStr.getString("ISBN"));
+			brDTO.setBr_title(rStr.getString("도서명"));
+
+			brDTO.setBr_price(rStr.getInt("대여금"));
+			brDTO.setBr_edate(rStr.getString("반납일"));
+
+			brList.add(brDTO);
+		}
+		return brList;
+	}
 
 	@Override
 	public List<BookRentDTO> selectAll() {
 		// TODO 전체리스트
-		
+
 		String sql = "SELECT * FROM view_도서대여정보";
 		PreparedStatement pStr = null;
-		
+
 		try {
 			pStr = dbConn.prepareStatement(sql);
-			
-			//메서드를 분류해줬기 때문에 필요없는 부분이 됨
+
+			// 메서드를 분류해줬기 때문에 필요없는 부분이 됨
 //			ResultSet rSet = pStr.executeQuery();
 //			
 //			List<BookRentDTO> brList = new ArrayList<BookRentDTO>();
@@ -80,7 +78,7 @@ public class BookRentServiceImplV1 implements BookRentService{
 			List<BookRentDTO> brList = this.select(pStr);
 			pStr.close();
 			return brList;
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -91,49 +89,49 @@ public class BookRentServiceImplV1 implements BookRentService{
 	@Override
 	public BookRentDTO findById(long seq) { // 매개변수로 받은 long seq값은 Controller에서 받은 String 값을 정수형으로 변환해서 받은 값 "id"
 		// TODO PK로 조회하기
-		
-		
+
 		/*
-		 * DB Injection 공격에 사용되는 매우 취약한 코드
-		 * 만약 매개변수로 직접 SQL 명령문을 다음과 같이 만들면
-		 * "WHERE 도서명 = " + title;
-		 * title 변수값에 " 1 OR 1 = 1 과 같은 문자열을 담아서 보내면
-		 * 실제 쿼리는 
+		 * DB Injection 공격에 사용되는 매우 취약한 코드 만약 매개변수로 직접 SQL 명령문을 다음과 같이 만들면
+		 * "WHERE 도서명 = " + title; title 변수값에 " 1 OR 1 = 1 과 같은 문자열을 담아서 보내면 실제 쿼리는
 		 * WHERE 도서명 1 or = 1 과 같이 만들어져 버린다.
 		 * 
-		 * Prepare... 를 사용할때는 조건문의 변수가 포함될 위치에 ? 기호를 사용한다.
-		 * WHERE 도서명 = ? 과 같이 사용한다.
-		 *  
+		 * Prepare... 를 사용할때는 조건문의 변수가 포함될 위치에 ? 기호를 사용한다. WHERE 도서명 = ? 과 같이 사용한다.
+		 * 
 		 */
-		
+
 		String sql = " SELECT * FROM view_도서대여정보";
 		sql += " WHERE 주문번호 = ? ";
-		
+
 		PreparedStatement pStr = null;
 		try {
 			// 아래의 2개 코드에 의해서 WHERE 주문번호 = 값 형식의 SQL이 만들어진다.
 			pStr = dbConn.prepareStatement(sql);
 			pStr.setLong(1, seq);
-			
+
 //			ResultSet rSet = pStr.executeQuery();
 //			if(rSet.next()) {
 //				// VO에 담기
 //			}
 //			rSet.close();
-			
+
 			// PK로 조회를 했기 때문에 List에는 1개밖에 데이터가 없다
 			// 배열의 시작값은 0!!
 			// list의 0번 데이터만 getter하여 DTO에 담는다
-			BookRentDTO brDTO = this.select(pStr).get(0);
+			List<BookRentDTO> brList = this.select(pStr);
+			BookRentDTO brDTO = null;
+
+			if (brList != null && brList.size() > 0) {
+				brDTO = brList.get(0);
+			}
+
 			pStr.close();
 			return brDTO;
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
 		return null;
 	}
 
@@ -142,13 +140,13 @@ public class BookRentServiceImplV1 implements BookRentService{
 		// TODO ISBN으로 조회
 		String sql = " SELECT * FROM view_도서대여정보 ";
 		sql += " WHERE ISBN = ?";
-		
-		PreparedStatement pStr = null; 
-		
+
+		PreparedStatement pStr = null;
+
 		try {
 			pStr = dbConn.prepareStatement(sql);
 			pStr.setString(1, isbn);
-			
+
 			List<BookRentDTO> brList = this.select(pStr);
 			pStr.close();
 			return brList;
@@ -156,7 +154,7 @@ public class BookRentServiceImplV1 implements BookRentService{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return null;
 	}
 
@@ -165,7 +163,7 @@ public class BookRentServiceImplV1 implements BookRentService{
 		// TODO 도서명으로 조회
 		String sql = " SELECT * FROM view_도서대여정보 ";
 		sql += " WHERE 도서명 LIKE '%' || ? || '%' ";
-		
+
 		PreparedStatement pStr = null;
 		try {
 			pStr = dbConn.prepareStatement(sql);
@@ -177,7 +175,7 @@ public class BookRentServiceImplV1 implements BookRentService{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return null;
 	}
 
@@ -208,9 +206,9 @@ public class BookRentServiceImplV1 implements BookRentService{
 	@Override
 	public int insert(BookRentVO bookRentVO) {
 		// TODO Auto-generated method stub
-		
+
 		// DB에 Insert한 후 DB안에 저장한 컬럼의 PK값을 불러와서 PK값을 다시 return시키기 위함 뇌피셜
-		// 
+		//
 		return 0;
 	}
 
@@ -225,7 +223,5 @@ public class BookRentServiceImplV1 implements BookRentService{
 		// TODO Auto-generated method stub
 		return 0;
 	}
-	
-	
 
 }
