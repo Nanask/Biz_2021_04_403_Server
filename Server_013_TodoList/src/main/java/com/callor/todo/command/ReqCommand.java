cac97ff4eb5364b33cd594ec90018db820f7ab14
Nanask <ybhag1005@daum.net>
2021-05-29 21:34:35 +0900

    todolist

--- Server_013_TodoList/src/main/java/com/callor/todo/command/ReqCommand.java
+++ Server_013_TodoList/src/main/java/com/callor/todo/command/ReqCommand.java
@@ -6,7 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class ReqCommend {
+public class ReqCommand {
 
 	private static final String prefix = "/WEB-INF/views/";
 	private static final String surfix = ".jsp";
--- Server_013_TodoList/src/main/java/com/callor/todo/config/DBContract.java
+++ Server_013_TodoList/src/main/java/com/callor/todo/config/DBContract.java
@@ -8,10 +8,17 @@ public class DBContract {
 
 	private static Connection dbConn;
 
+	/*
+	 * static 초기화 블럭 static으로 선언된 변수나 객체를 프로젝트가 시작될때 자동으로 생성하는 코드를 작성하는 부분
+	 * 
+	 * Connection 객체인 dbConn을 사용할 준비를 하기 DB Server와 연동을 시작하여 연결 session 을 생성해둔다 필요할때
+	 * get() method를 통하여 제공해준다
+	 */
+
 	static {
 
 		String jdbcDriver = "com.mysql.cj.jdbc.Driver";
-		String url = "jdbc:mysql://127.0.0.1:3306/mydb";
+		String url = "jdbc:mysql://127.0.0.1:3306/guestbook";
 		String user = "gbUser";
 		String password = "12345";
 
@@ -30,6 +37,10 @@ public class DBContract {
 		}
 	}
 
+	// get Instance method
+	// Instance : 사용준비(생성된, 초기화된) 객체
+	// private static 으로 선언된 변수, 객체를
+	// 다른 곳에서 사용할수 있도록 제공하는 method
 	public static Connection getDbConn() {
 		return dbConn;
 	}
